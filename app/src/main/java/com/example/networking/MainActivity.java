e9736cd21453593bf37f71c44997b4786d5bd2c4
a22eriwi <a22eriwi@student.his.se>
2023-04-27 11:32:07 +0200

    La till nya klasser, recycler views och recycler adapter

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 @SuppressWarnings("FieldCanBeLocal")
+
+
+
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+
+    private ArrayList<Mountain> Mountains;
+    private RecyclerViewAdapter Adapter;
+
+    RecyclerView view = findViewById(R.id.recyclerView);
+view.setLayoutManager(new LinearLayoutManager(this));
+view.setAdapter(adapter);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
